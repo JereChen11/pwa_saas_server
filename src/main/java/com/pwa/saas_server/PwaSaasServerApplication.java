ea27ef81ffa18d67f37da86ced4d15e572107336
JereChen <jerechen11@gmail.com>
2024-07-14 22:06:32 +0800

    init project

---
+++ src/main/java/com/pwa/saas_server/PwaSaasServerApplication.java
@@ -0,0 +1,13 @@
+package com.pwa.saas_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwaSaasServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PwaSaasServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pwa/saas_server/PwaSaasServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwa.saas_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwaSaasServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
