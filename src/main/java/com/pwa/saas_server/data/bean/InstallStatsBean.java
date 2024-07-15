ea5c852c07ef6b3d766df0ae4803b2d994a25146
JereChen <jerechen11@gmail.com>
2024-07-15 16:29:43 +0800

    add user_browser and is_inst_success attr

--- src/main/java/com/pwa/saas_server/data/bean/InstallStatsBean.java
+++ src/main/java/com/pwa/saas_server/data/bean/InstallStatsBean.java
@@ -17,8 +17,11 @@ public class InstallStatsBean {
     private Long instId; //安装表ID，主键
     private String userInstIp; //用户安装IP
     private String userAgent; //用户ua
+    private String userBrowserType; //用户浏览器类型，例如：chrome，safari，opera..
+    private String userBrowserVersion; //用户浏览器版本号，例如：chrome 116
     private String userLanguage; //用户浏览器语言
     private String userNetType; //用户的网络连接类型（如 WiFi 4G）
+    private Boolean isInstSuccess; //是否安装成功
     private String customParams; //根据业务需求的自定义参数
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Timestamp createdTime; //创建时间
